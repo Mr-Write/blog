73282bb95cb9240edc057c38abc08e0daff51a7e
white <3179167073@qq.com>
2023-07-29 01:29:01 +0800

    try deploy

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"icon\":\"home\",\"heroImage\":\"/logo.png\",\"heroText\":\"狐狸半面添\",\"tagline\":\"所有真心的痴心的话仍在我心中，虽然已没有她\",\"description\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://mister-hope.github.io/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"狐狸半面添的客栈\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"狐狸半面添\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"\\\"}\"]]},\"headers\":[],\"readingTime\":{\"minutes\":0.3,\"words\":90},\"filePathRelative\":\"README.md\",\"excerpt\":\"\",\"copyright\":{\"author\":\"狐狸半面添\"},\"autoDesc\":true}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"icon\":\"home\",\"heroImage\":\"/logo.png\",\"heroText\":\"狐狸半面添\",\"tagline\":\"所有真心的痴心的话仍在我心中，虽然已没有她\",\"description\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://mister-hope.github.io/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"狐狸半面添的客栈\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2023-07-28T16:31:16.000Z\"}],[\"meta\",{\"property\":\"article:author\",\"content\":\"狐狸半面添\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2023-07-28T16:31:16.000Z\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"\\\"}\"]]},\"headers\":[],\"git\":{\"createdTime\":1690561876000,\"updatedTime\":1690561876000,\"contributors\":[{\"name\":\"white\",\"email\":\"3179167073@qq.com\",\"commits\":1}]},\"readingTime\":{\"minutes\":0.3,\"words\":90},\"filePathRelative\":\"README.md\",\"localizedDate\":\"2023年7月29日\",\"excerpt\":\"\",\"copyright\":{\"author\":\"狐狸半面添\"},\"autoDesc\":true}")
