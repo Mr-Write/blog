e753b05020e5f113f51ece8488992b3f583190a4
white <3179167073@qq.com>
2023-08-05 01:35:52 +0800

    updated

--- docs/.vuepress/.temp/internal/pagesData.js
+++ docs/.vuepress/.temp/internal/pagesData.js
@@ -3,6 +3,8 @@ export const pagesData = {
   "v-8daa1a0e": () => import(/* webpackChunkName: "v-8daa1a0e" */"D:/SoftwareEngineering/docs/blog/docs/.vuepress/.temp/pages/index.html.js").then(({ data }) => data),
   // path: /basic/the-test.html
   "v-370e5d1e": () => import(/* webpackChunkName: "v-370e5d1e" */"D:/SoftwareEngineering/docs/blog/docs/.vuepress/.temp/pages/basic/the-test.html.js").then(({ data }) => data),
+  // path: /basic/beginner-guide-2023/
+  "v-9d550604": () => import(/* webpackChunkName: "v-9d550604" */"D:/SoftwareEngineering/docs/blog/docs/.vuepress/.temp/pages/basic/beginner-guide-2023/index.html.js").then(({ data }) => data),
   // path: /basic/beginner-guide-2022/1-what-is-program-language.html
   "v-4ca7f3ba": () => import(/* webpackChunkName: "v-4ca7f3ba" */"D:/SoftwareEngineering/docs/blog/docs/.vuepress/.temp/pages/basic/beginner-guide-2022/1-what-is-program-language.html.js").then(({ data }) => data),
   // path: /basic/beginner-guide-2022/2-What-is-C-and-my-suggestions.html
@@ -21,8 +23,6 @@ export const pagesData = {
   "v-378eebf2": () => import(/* webpackChunkName: "v-378eebf2" */"D:/SoftwareEngineering/docs/blog/docs/.vuepress/.temp/pages/basic/beginner-guide-2022/8-answer-some-other-questions.html.js").then(({ data }) => data),
   // path: /basic/beginner-guide-2022/
   "v-9d550642": () => import(/* webpackChunkName: "v-9d550642" */"D:/SoftwareEngineering/docs/blog/docs/.vuepress/.temp/pages/basic/beginner-guide-2022/index.html.js").then(({ data }) => data),
-  // path: /basic/beginner-guide-2023/
-  "v-9d550604": () => import(/* webpackChunkName: "v-9d550604" */"D:/SoftwareEngineering/docs/blog/docs/.vuepress/.temp/pages/basic/beginner-guide-2023/index.html.js").then(({ data }) => data),
   // path: /404.html
   "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"D:/SoftwareEngineering/docs/blog/docs/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
   // path: /basic/
--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,7 @@
 ---
 home: true
 icon: home
-heroImage: /assets/base_images/author.png
+heroImage: https://sangxin-tian.oss-cn-nanjing.aliyuncs.com/image/1691082121450.png
 heroText: 狐狸半面添
 tagline: 愿你编码半生，归来仍是少年
 # heroFullScreen: true
